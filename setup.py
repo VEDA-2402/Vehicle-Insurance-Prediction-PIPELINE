fe76e26a39ef067e923395d849ac709c3846f04c
T.Veda <veda.vedhya240205@gmail.com>
2025-11-25 00:29:01 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Veda",
+    author_email="veda.vedhya240205@gmail.com",
+    packages=find_packages()
+)
